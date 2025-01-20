34d17af32a68703e59c85da58647cc3f038e4a17
Lucas0604 <139821991+Lucas0604@users.noreply.github.com>
2025-01-20 11:30:25 -0300

    comentarios em JS

---
+++ JS/Aula04/comentarios.js
@@ -0,0 +1,13 @@
+// com essas barras fazemos comentarios em linhas
+console.log('Olá Mundo!'); // comentarios são ignorados 
+console.log('Linha 1');
+console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
+
+/**
+ * E assim 
+ * fazemos
+ * comentarios 
+ * em blocos
+ */
