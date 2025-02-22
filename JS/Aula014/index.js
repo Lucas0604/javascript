43606faaefcbdb1bc51eb5538d8065f86e87dae9
Lucas0604 <139821991+Lucas0604@users.noreply.github.com>
2025-02-22 00:07:03 -0300

    estudando Number e Math em Js

---
+++ JS/Aula014/index.js
@@ -0,0 +1,16 @@
+// let num1 = 12;
+// let num2 = 2.23212313231;
+
+// // console.log(num1.toString() + num2.toString());
+// // num1 = num1.toString();
+// // console.log(typeof num1);
+// // console.log(num2.toFixed(2))
+// let temp = num1 + '6';
+// console.log(temp);
+
+let num1 = 0.7;
+let num2 = 0.1;
+
+num1 += num2;
+
+console.log(Number(num1.toFixed(2   )))
